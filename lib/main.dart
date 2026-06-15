689fdae2c453956fce30212d798d86e70acc0a9b
yamdev07 <yamdyamd080@gmail.com>
2026-06-15 18:00:26 +0100

    feat(app): init Hive and fr_FR locale on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'app.dart';
+import 'data/hive_service.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('fr_FR');
+  await HiveService.init();
   runApp(const MoneyTrackApp());
 }
